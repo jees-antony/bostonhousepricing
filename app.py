df6d826ed1ca9e8eb86de95efe7c710d13ee4000
jees_antony <jeesantony019@gmail.com>
2024-02-22 23:20:12 +0530

    fast api app tested

--- app.py
+++ app.py
@@ -1,39 +1,45 @@
-import json
-import pickle
-
-from flask import Flask,request,app,jsonify,url_for,render_template
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import FileResponse
+from typing import List
+import shutil
+import torch
+from PIL import Image
 import numpy as np
-import pandas as pd
+from io import BytesIO
+import cv2
 
-app=Flask(__name__)
-## Load the model from local dir
-regmodel=pickle.load(open('regmodel.pkl','rb'))
-scalar=pickle.load(open('scaling.pkl','rb'))
-@app.route('/')
-def home():
-    return render_template('home.html')
+app = FastAPI()
 
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
-    output=regmodel.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=regmodel.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+# Load YOLOv5 model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
 
+def detect_objects(image_path: str) -> np.ndarray:
+    # Perform object detection
+    results = model(image_path)
 
-if __name__=="__main__":
-    app.run(debug=True)
-   
-     
+    # Render bounding boxes on the image
+    image_with_boxes = results.render()[0]
+
+    return image_with_boxes
+
+
+@app.post("/detect")
+async def detect_and_return_image(files: List[UploadFile] = File(...)):
+    # Check if there's only one file uploaded
+    if len(files) != 1:
+        raise HTTPException(status_code=400, detail="Exactly one file should be uploaded.")
+
+    # Save the uploaded image temporarily
+    uploaded_file = files[0]
+    with open(uploaded_file.filename, "wb") as buffer:
+        shutil.copyfileobj(uploaded_file.file, buffer)
+
+    # Detect objects in the uploaded image
+    image_with_boxes = detect_objects(uploaded_file.filename)
+
+    # Convert numpy array to bytes
+    image_bytes = cv2.imencode('.jpg', image_with_boxes)[1].tobytes()
+
+    # Return the image with bounding boxes
+    return FileResponse(BytesIO(image_bytes), media_type="image/jpeg")
