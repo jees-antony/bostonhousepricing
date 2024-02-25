e97551b784b168198af28a6a3a6453753665075b
jees_antony <jeesantony019@gmail.com>
2024-02-25 17:23:30 +0530

    light version checking with only fast api hello world

--- app.py
+++ app.py
@@ -5,44 +5,47 @@ from PIL import Image
 import numpy as np
 from io import BytesIO
 from pathlib import Path
-from ultralytics import YOLO
+# from ultralytics import YOLO
 
 app = FastAPI()
 
 # Load YOLOv8 pretrained model
-model = YOLO('best.pt')
+# model = YOLO('best.pt')
 
-print("running from new app")
-def detect_objects(image):
-    # Perform object detection
-    results = model(image)
+# print("running from new app")
+# def detect_objects(image):
+#     # Perform object detection
+#     results = model(image)
 
-    # Check if results is empty
-    if not results:
-        return None
+#     # Check if results is empty
+#     if not results:
+#         return None
 
-    # Render bounding boxes on the image
-    rendered_images = [result.render()[0] for result in results]
+#     # Render bounding boxes on the image
+#     rendered_images = [result.render()[0] for result in results]
 
-    # Convert numpy arrays to PIL Images
-    images_with_boxes = [Image.fromarray(image_with_boxes) for image_with_boxes in rendered_images]
+#     # Convert numpy arrays to PIL Images
+#     images_with_boxes = [Image.fromarray(image_with_boxes) for image_with_boxes in rendered_images]
 
+#     return images_with_boxes
 
-    return images_with_boxes
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
-@app.post("/detect")
-async def detect_and_return_image(file: UploadFile = File(...)):
+# @app.post("/detect")
+# async def detect_and_return_image(file: UploadFile = File(...)):
     
-    # Read the uploaded image
-    contents = await file.read()
-    image = Image.open(BytesIO(contents))
+#     # Read the uploaded image
+#     contents = await file.read()
+#     image = Image.open(BytesIO(contents))
 
-    # Detect objects in the uploaded image
-    image_with_boxes = detect_objects(image)
+#     # Detect objects in the uploaded image
+#     image_with_boxes = detect_objects(image)
 
-    # Save the image with bounding boxes
-    output_image_path = "image_with_boxes.jpg"
-    image_with_boxes.save(output_image_path)
+#     # Save the image with bounding boxes
+#     output_image_path = "image_with_boxes.jpg"
+#     image_with_boxes.save(output_image_path)
 
-    # Return the image with bounding boxes
-    return FileResponse(output_image_path)
+#     # Return the image with bounding boxes
+#     return FileResponse(output_image_path)
