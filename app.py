7b99160287af83da38d460d0ae4232f10ee17483
jees_antony <jeesantony019@gmail.com>
2024-03-01 22:15:45 +0530

    seperate file prediction.py

--- app.py
+++ app.py
@@ -2,20 +2,12 @@ from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 from typing import List
-from PIL import Image, ImageDraw
-import numpy as np
+from PIL import Image
 from io import BytesIO
-from pathlib import Path
-from ultralytics import YOLO
-from starlette.responses import StreamingResponse
-import json
-import base64
-# 
-
-import os
-os.environ['OPENCV_IO_ENABLE_JASPER'] = 'true'  # Set environment variable
+import detect   #detect.py
 
 app = FastAPI()
+
 # Allo request from all origins -CORS
 app.add_middleware(
     CORSMiddleware,
@@ -24,11 +16,8 @@ app.add_middleware(
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
-templates = Jinja2Templates(directory="templates")
-# Load YOLOv8 pretrained model
-# model = YOLO('best (2).pt')
 
-print("running from new app")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def home(request: Request):
@@ -46,70 +35,10 @@ async def detect_and_return_image(image_file: UploadFile = File(...)):
     [[x1,y1,x2,y2,object_type,probability],..]
     """
     buf = await image_file.read()
-    boxes, class_prob = detect_objects_on_image(Image.open(BytesIO(buf)))
+    boxes, class_prob = detect.detect_objects_on_image(Image.open(BytesIO(buf)))
     print(f'class proba {class_prob}')
-    annotated_image = annotate_image(Image.open(BytesIO(buf)), boxes)
+    annotated_image = detect.annotate_image(Image.open(BytesIO(buf)), boxes)
     return {
-        "annotated_image": image_to_base64(annotated_image),
+        "annotated_image": detect.image_to_base64(annotated_image),
         "class_prob": class_prob
     }
-
-
-def detect_objects_on_image(image):
-    """
-    Function receives an image,
-    passes it through YOLOv8 neural network
-    and returns an array of detected objects
-    and their bounding boxes
-    :param image: Input image
-    :return: Array of bounding boxes in format
-    [[x1,y1,x2,y2,object_type,probability],..]
-    """
-    model = YOLO('best (2).pt')
-    results = model.predict(image)
-    result = results[0]
-    output = []
-    class_prob = []
-    for box in result.boxes:
-        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
-        class_id = box.cls[0].item()
-        prob = round(box.conf[0].item(), 2)
-        output.append([x1, y1, x2, y2, result.names[class_id], prob])
-        class_prob.append([result.names[class_id], prob])
-    return output, class_prob
-
-
-def annotate_image(image, boxes):
-    """
-    Function annotates the image with bounding boxes.
-    :param image: Input image
-    :param boxes: Array of bounding boxes in format
-    [[x1,y1,x2,y2,object_type,probability],..]
-    :return: Annotated image
-    """
-    # Draw bounding boxes on the image
-    draw = ImageDraw.Draw(image)
-    for box in boxes:
-        x1, y1, x2, y2, object_type, probability = box
-        draw.rectangle([(x1, y1), (x2, y2)], outline="red", width=3)
-        draw.text((x1, y1 - 10), f"{object_type} ({probability})", fill="red")
-
-    return image
-
-
-def save_annotated_image(image):
-    """
-    Function saves the annotated image and returns
-    the image as a response.
-    :param image: Annotated image
-    :return: StreamingResponse with the image
-    """
-    output_buffer = BytesIO()
-    image.save(output_buffer, format="PNG")
-    output_buffer.seek(0)
-    return StreamingResponse(output_buffer, media_type="image/png")
-
-def image_to_base64(image):
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    return base64.b64encode(buffered.getvalue()).decode('utf-8')
---
+++ detect.py
@@ -0,0 +1,64 @@
+from ultralytics import YOLO
+from PIL import ImageDraw
+from io import BytesIO
+from starlette.responses import StreamingResponse
+import base64
+
+def detect_objects_on_image(image):
+    """
+    Function receives an image,
+    passes it through YOLOv8 neural network
+    and returns an array of detected objects
+    and their bounding boxes
+    :param image: Input image
+    :return: Array of bounding boxes in format
+    [[x1,y1,x2,y2,object_type,probability],..]
+    """
+    model = YOLO('best (2).pt')
+    results = model.predict(image)
+    result = results[0]
+    output = []
+    class_prob = []
+    for box in result.boxes:
+        x1, y1, x2, y2 = [round(x) for x in box.xyxy[0].tolist()]
+        class_id = box.cls[0].item()
+        prob = round(box.conf[0].item(), 2)
+        output.append([x1, y1, x2, y2, result.names[class_id], prob])
+        class_prob.append([result.names[class_id], prob])
+    return output, class_prob
+
+
+def annotate_image(image, boxes):
+    """
+    Function annotates the image with bounding boxes.
+    :param image: Input image
+    :param boxes: Array of bounding boxes in format
+    [[x1,y1,x2,y2,object_type,probability],..]
+    :return: Annotated image
+    """
+    # Draw bounding boxes on the image
+    draw = ImageDraw.Draw(image)
+    for box in boxes:
+        x1, y1, x2, y2, object_type, probability = box
+        draw.rectangle([(x1, y1), (x2, y2)], outline="red", width=3)
+        draw.text((x1, y1 - 10), f"{object_type} ({probability})", fill="red")
+
+    return image
+
+
+def save_annotated_image(image):
+    """
+    Function saves the annotated image and returns
+    the image as a response.
+    :param image: Annotated image
+    :return: StreamingResponse with the image
+    """
+    output_buffer = BytesIO()
+    image.save(output_buffer, format="PNG")
+    output_buffer.seek(0)
+    return StreamingResponse(output_buffer, media_type="image/png")
+
+def image_to_base64(image):
+    buffered = BytesIO()
+    image.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode('utf-8')
